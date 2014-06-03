f9e0dccd6e5450484e0b526bc0808cbfa3753954
Gorea235 <lkdvs3@gmail.com>
2014-06-03 21:21:28 +0100

    Fixed syntax errors in the executor

--- src/main/java/gsmg/plugin/gsmg_base/gsmg_base_executor.java
+++ src/main/java/gsmg/plugin/gsmg_base/gsmg_base_executor.java
@@ -6,27 +6,27 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class gsmg_base_executor implements CommandExecutor {
-  
-        public boolean onCommand(CommandSender sender, Command cmd, String label,
-                			String[] args) {
-                		if (sender instanceof Player) {
-                			Player player = (Player) sender;
-                			String arg1 = args[0].toLowerCase();
-                			if(arg1 == "help") {
-                			        sender.sendMessage("----GSMG Command List----");
-                			        sender.sendMessage("/GSMG Close (Minigame)");
-                			        sender.sendMessage("/GSMG (Minigame) (Command)");
-                			        sender.sendMessage("/GSMG Start (Minigame)")
-                			        sender.sendMessage("")
-                			} else if (arg1 == "close") {
-                		  
-                			} else {
-                			        return false; 
-                			}
-                			return true;  
-              } else {
-                      sender.sendMessage("The console cannot use these commands.");
-              }
-              return false;
-}
 
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+		if (sender instanceof Player) {
+			Player player = (Player) sender;
+			String arg1 = args[0].toLowerCase();
+			if (arg1 == "help") {
+				sender.sendMessage("----GSMG Command List----");
+				sender.sendMessage("/GSMG Close (Minigame)");
+				sender.sendMessage("/GSMG (Minigame) (Command)");
+				sender.sendMessage("/GSMG Start (Minigame)");
+				sender.sendMessage("");
+			} else if (arg1 == "close") {
+
+			} else {
+				return false;
+			}
+			return true;
+		} else {
+			sender.sendMessage("The console cannot use these commands.");
+		}
+		return false;
+	}
+}
