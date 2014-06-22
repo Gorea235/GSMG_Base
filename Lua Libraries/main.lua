7343159ea0ae25b50f94aaaac8e345b51ee20335
Gorea235 <lkdvs3@gmail.com>
2014-06-22 20:50:32 +0100

    Added a Lua library for some needed functions
    This is a file I have been working on, and it contains Lua functions to
    make general coding easier. The PrintTable function (inspired by Gmod)
    will print the entire contents of a table to the console. The wait
    function (inspired by ROBLOX) will wait for the amount of seconds. It
    should be rarely, if at all, used. It will make the entire server stop,
    which is not good, so avoid using it. The table.tostring and
    table.fromstring (inspired by ComputerCraft & Gmod) will convert a table
    to a full string, and back into a table. This allows for printing
    (although, PrintTable does a better job of this) and for saving to a
    file.

---
+++ Lua Libraries/main.lua
@@ -0,0 +1,58 @@
+function PrintTable ( tbl )
+  local level = 0
+  local setSpaces = function (num, msg)
+    pre = ""
+    num = num - 1
+    for i = 0, num do
+      pre = pre.."  "
+    end
+    return pre..msg
+  end
+  print(setSpaces(level, tostring(tbl)))
+  level = level + 1
+  for i, k in pairs(tbl) do
+    if type(k) == "table" then
+      PrintTable(k)
+    else
+      print(setSpaces(level, tostring(i).." = "..tostring(k)))
+    end
+  end
+  level = level - 1
+end
+
+function wait ( secs )
+  local t0 = os.clock()
+  while os.clock() - t0 <= secs do end
+end
+
+table.tostring = function ( tbl )
+  local str = "{"
+  local comma = ""
+  for k, v in pairs(tbl) do
+    local key = nil
+    if type(k) == "string" then
+      key = k
+    elseif type(k) == "number" or type(k) == "boolean" or type(k) == "function" then
+      key = tostring(k)
+    end
+    local value = nil
+    if type(v) == "string" then
+      value = v
+    elseif type(v) == "number" or type(v) == "boolean" then
+      value = tostring(v)
+    elseif type(v) == "table" then
+      value = table.tostring(v)
+    end
+    if key ~= nil and value ~= nil then
+      str = str..comma.." [\""..key.."\"] = \""..value.."\""
+    end
+    comma = ","
+  end
+  str = str.." }"
+  return str
+end
+
+table.fromstring = function ( str )
+  str = "return "..str
+  return assert(load(str))()
+end
