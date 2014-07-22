f6897cb5f6fe123295ab5ed2ff9c7f807672102b
Gorea235 <lkdvs3@gmail.com>
2014-07-22 14:08:40 +0100

    Localised all variables in Template
    If a variable is global , any other lua script can access it (as long as
    the file with the variable in was initialised before the file accessing
    the variable). The template now has all it's variables localised.

--- Lua/Minigames/Template/Template.lua
+++ Lua/Minigames/Template/Template.lua
@@ -1,23 +1,24 @@
-name = "Template" -- name of minigame, allows for better continuity  
-location = MiniGamePath..name -- the directory containting this file, try to match cases 
+-- Remember to localise all variables (even in functions)! Otherwise you can interfere with other minigames!
+local name = "Template" -- name of minigame, allows for better continuity  
+local location = MiniGamePath..name -- the directory containting this file, try to match cases 
                                 -- (as far as I can tell, directories in Lua are case-insensitive)
 
 require( IncludePath.."main" ) -- adds the Lua-written library to this file, all functions are now directly accessable
-minigame = require ( class.minigame ) -- requires the minigame library
-event = require ( class.event ) -- requires the event library
-chatcolours = require ( class.chatcolours ) -- requires the chatcolours library. 'colour' can be spelt 'color' where ever you want
+local minigame = require ( class.minigame ) -- requires the minigame library
+local event = require ( class.event ) -- requires the event library
+local chatcolours = require ( class.chatcolours ) -- requires the chatcolours library. 'colour' can be spelt 'color' where ever you want
                                               -- e.g. 'chatcolors = require ( class.chatcolors )' would work too (I'm English, that is why I prefer 'colour')
-eventLocation = location..separator.."events"..separator -- the directory containing the event-handling lua scripts
-commandEvent = loadlibrary( eventLocation.."command.lua" ) -- stores the return from the file (by default a function)
-signEvent = loadlibrary( eventLocation.."sign.lua" ) -- stores the return from the file (by default a function)
-playerJoinEvent = loadlibrary( eventLocation.."playerJoin.lua" ) -- stores the return from the file (by default a function)
-playerLeaveEvent = loadlibrary( eventLocation.."playerLeave.lua" ) -- stores the return from the file (by default a function)
-playerChatEvent = loadlibrary( eventLocation.."playerChat.lua" ) -- stores the return from the file (by default a function)
-playerDeathEvent = loadlibrary( eventLocation.."playerDeath.lua" ) -- stores the return from the file (by default a function)
-playerRespawnEvent = loadlibrary( eventLocation.."playerRespawn.lua" ) -- stores the return from the file (by default a function)
-shutdownEvent = loadlibrary( eventLocation.."shutdown.lua" ) -- stores the return from the file (by default a function)
+local eventLocation = location..separator.."events"..separator -- the directory containing the event-handling lua scripts
+local commandEvent = loadlibrary( eventLocation.."command.lua" ) -- stores the return from the file (by default a function)
+local signEvent = loadlibrary( eventLocation.."sign.lua" ) -- stores the return from the file (by default a function)
+local playerJoinEvent = loadlibrary( eventLocation.."playerJoin.lua" ) -- stores the return from the file (by default a function)
+local playerLeaveEvent = loadlibrary( eventLocation.."playerLeave.lua" ) -- stores the return from the file (by default a function)
+local playerChatEvent = loadlibrary( eventLocation.."playerChat.lua" ) -- stores the return from the file (by default a function)
+local playerDeathEvent = loadlibrary( eventLocation.."playerDeath.lua" ) -- stores the return from the file (by default a function)
+local playerRespawnEvent = loadlibrary( eventLocation.."playerRespawn.lua" ) -- stores the return from the file (by default a function)
+local shutdownEvent = loadlibrary( eventLocation.."shutdown.lua" ) -- stores the return from the file (by default a function)
 
-this = minigame.register( name ) -- registers the minigame name 
+local this = minigame.register( name ) -- registers the minigame name 
                                    --(only exists until the server or lua restarts, and can only be done once with each name)
 -- Minigame specific events, only one handler per minigame, and they link directly to the minigame, so no UID (Unique Identifier) needed
 this.hookEvent.OnCommand( commandEvent ) -- hooks the minigame command to the handler function
